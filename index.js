6999c0fc98284d724a6ed4501e14a5a3fa2828d3
Azzam ALmasslamany <azzammeri24@gmail.com>
2023-09-28 18:50:15 +0300

    Full Code Here

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+// function countDown(num) {
+//   while (num >= 0) {
+//     console.log(num);
+//     num--;
+//   }
+// }
+
+
+function countDown(num) {
+  let count = 0;
+  while (num >= 0) {
+    console.log(num);
+    num--;
+    count++;
+  }
+  console.log("Count:", count);
+}
+
+countDown(10);
+
+countDown(10);
